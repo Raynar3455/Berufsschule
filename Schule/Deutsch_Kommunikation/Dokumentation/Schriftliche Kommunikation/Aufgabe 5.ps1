ed9e16488fa9ee9cdc0dba10bc94152d5d4dad84
Marvin Lennartz <marvin.lennartz@inform-software.com>
2019-10-08 09:09:39 +0200

    08.10

---
+++ Schule/Deutsch_Kommunikation/Dokumentation/Schriftliche Kommunikation/Aufgabe 5.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+$curdate = Get-Date -Format "dd.MM.yyyy"
+$tag = (Get-Date).DayOfWeek  
+$hostname = $env:USERNAME
+$Zeit = (Get-Date).ToString("HH:MM:ss")
+$ip = Get-WmiObject win32_systemNetworkConnections
+$computerinfo = Get-WmiObject 
+$Bios = Get-WmiObject win32_BIOS
+Write-Host "Hallo $env:USERNAME !"
+Write-Host "Heute ist $tag der $curdate es ist $Zeit "
+Write-Host "Host: $hostname"
+Write-Host "IP: $ip"
+Write-Host "Computerinfo: $computerinfo"
+Write-Host "Bios: $Bios"
